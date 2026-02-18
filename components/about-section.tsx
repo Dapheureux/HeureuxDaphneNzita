d68ac93ea29832e5a901417b199b82ba2fb86703
Dapheureux <dnzita4@gmail.com>
2026-02-18 10:36:38 +0100

    18/02/2026 3

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -64,7 +64,7 @@ export function AboutSection() {
               <Camera className="h-16 w-16 text-accent" />
               <Image src="/Daph.png" alt={"Daph"}   width={256} height={256} className="mb-3"></Image>
               <div className="absolute inset-0 rounded-full bg-black/20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                <span className="text-white font-outfit">Heureux Daphné N'ZITA</span>
+                <span className="text-white font-outfit">Heureux Daphné N&apos;ZITA</span>
               </div>
             </div>
           </div>
